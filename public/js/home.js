4544da6bf92bc506503650a94501cb53c31400f7
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2025-01-27 19:37:14 +0700

    Home Section 1

--- public/js/home.js
+++ public/js/home.js
@@ -5,64 +5,178 @@ document.addEventListener("DOMContentLoaded", () => {
   const textKanan = document.getElementById("text-kanan");
   const waveSvg = document.getElementById("intro");
 
+  // responsive
+  const breakpoints = {
+    smallPhone: "(max-width: 480px)",          // Smartphone kecil
+    largePhone: "(min-width: 481px) and (max-width: 767px)", // Smartphone besar
+    tablet: "(min-width: 768px) and (max-width: 1024px)",    // Tablet
+    smallLaptop: "(min-width: 1025px) and (max-width: 1440px)", // Laptop sedang
+    desktop: "(min-width: 1441px) and (max-width: 1920px)",     // Desktop
+    largeMonitor: "(min-width: 1921px)",       // Monitor besar
+  };
+
+
+  // --------------
+  // section 1
+  // --------------
+
   // animated
   // Animasi teks "OSPEK"
-  gsap.fromTo(
-    textKiri,
-    // Posisi awal (kiri agak atas, transparan)
-    { x: -600, 
-      y: -100,  
-    }, 
-    // Posisi akhir (tengah, terlihat)
-    { x: 0, 
-      y: 0,  
-      duration: 2, 
-      delay: 2.3, 
-      ease: "power2.out" } 
-  );
+  gsap.matchMedia().add(breakpoints.desktop, () => {
+    gsap.fromTo(
+      textKiri,
+      // Posisi awal (kiri agak atas, transparan)
+      { x: -1400, 
+        y: -300,  
+      }, 
+      // Posisi akhir (tengah, terlihat)
+      { x: -500, 
+        y: 0,  
+        duration: 2, 
+        delay: 2.3, 
+        ease: "power2.out" } 
+    );
+  });
 
+  gsap.matchMedia().add(breakpoints.smallLaptop, () => {
+    gsap.fromTo(textKiri, {x: -900, y: -150}, {x: -350, y:0, duration: 2, delay: 2.3, ease: "power2.out"});
+  });
+    
+  gsap.matchMedia().add(breakpoints.tablet, () => {
+    gsap.fromTo(textKiri, {x: -700, y: -100}, {x: -280, y:0, duration: 2, delay: 2.3, ease: "power2.out"});
+  });
+
+  gsap.matchMedia().add(breakpoints.largePhone, () => {
+    gsap.fromTo(textKiri, {x: -550, y: -70}, {x: -200, y:0, duration: 2, delay: 2.3, ease: "power2.out"});
+  });
+
+  gsap.matchMedia().add(breakpoints.smallPhone, () => {
+    gsap.fromTo(textKiri, {x: -250, y: -80}, {x: -70, y:-170, duration: 2, delay: 2.3, ease: "power2.out"});
+  });
+    
   // Animasi teks "2025"
-  gsap.fromTo(
-    textKanan,
-    // Posisi awal (kanan agak atas, transparan)
-    { x: 600, 
-      y: -100,  
-    }, 
-    // Posisi akhir (tengah, terlihat)
-    { x: 0, 
-      y: 0,  
-      duration: 2, 
-      delay: 2.3, 
-      ease: "power2.out"} 
-  );
+  gsap.matchMedia().add(breakpoints.desktop, () => {
+    gsap.fromTo(
+      textKanan,
+      // Posisi awal (kanan agak atas, transparan)
+      { x: 1400, 
+        y: -300,  
+      }, 
+      // Posisi akhir (tengah, terlihat)
+      { x: 500, 
+        y: 0,  
+        duration: 2, 
+        delay: 2.3, 
+        ease: "power2.out"} 
+    );
+  });
+
+  gsap.matchMedia().add(breakpoints.smallLaptop, () => {
+    gsap.fromTo(textKanan, {x: 900, y: -150}, {x: 350, y:0, duration: 2, delay: 2.3, ease: "power2.out"});
+  });
+
+  gsap.matchMedia().add(breakpoints.tablet, () => {
+    gsap.fromTo(textKanan, {x: 700, y: -100}, {x: 280, y:0, duration: 2, delay: 2.3, ease: "power2.out"});
+  });
+
+  gsap.matchMedia().add(breakpoints.largePhone, () => {
+    gsap.fromTo(textKanan, {x: 550, y: -70}, {x: 200, y:0, duration: 2, delay: 2.3, ease: "power2.out"});
+  });
+
+  gsap.matchMedia().add(breakpoints.smallPhone, () => {
+    gsap.fromTo(textKanan, {x: 250, y: -60}, {x: 70, y:-170, duration: 2, delay: 2.3, ease: "power2.out"});
+  });
+
 
   // logo ospek
-  gsap.fromTo(
-    logo,
-    { scale: 2 }, // Starting state (besar)
-    { 
-      scale: 1.2,       // Ending state (kecil)
-      duration: 5,    // Durasi animasi mengecil
-      delay: 1.5,       // Waktu berhenti sebelum mulai mengecil
-      ease: "power2.out" 
-    }
-  );
+  // gsap.fromTo(
+  //   logo,
+  //   { scale: 2 }, // Starting state (besar)
+  //   { 
+  //     scale: 1.2,       // Ending state (kecil)
+  //     duration: 5,    // Durasi animasi mengecil
+  //     delay: 1.5,       // Waktu berhenti sebelum mulai mengecil
+  //     ease: "power2.out" 
+  //   }
+  // );
+
+  // Tambahkan animasi berdasarkan breakpoint
+  gsap.matchMedia().add(breakpoints.smallPhone, () => {
+    gsap.fromTo(logo, { scale: 1.3 }, { scale: 0.9, duration: 5, delay: 1.5, ease: "power2.out" });
+  });
+
+  gsap.matchMedia().add(breakpoints.largePhone, () => {
+    gsap.fromTo(logo, { scale: 1.8 }, { scale: 1, duration: 5, delay: 1.5, ease: "power2.out" });
+  });
+
+  gsap.matchMedia().add(breakpoints.tablet, () => {
+    // console.log(alert('tablet'));
+    gsap.fromTo(logo, { scale: 1.8 }, { scale: 1.2, duration: 5, delay: 1.5, ease: "power2.out" });
+  });
+
+  gsap.matchMedia().add(breakpoints.smallLaptop, () => {
+    // console.log(alert('layar desktop'));
+    gsap.fromTo(logo, { scale: 2 }, { scale: 1.5, duration: 5, delay: 1.5, ease: "power2.out" });
+  });
+
+  gsap.matchMedia().add(breakpoints.desktop, () => {
+    // console.log(alert('layar desktop'));
+    gsap.fromTo(logo, { scale: 2.8 }, { scale: 1.8, duration: 5, delay: 1.5, ease: "power2.out" });
+  });
+
+  gsap.matchMedia().add(breakpoints.largeMonitor, () => {
+    gsap.fromTo(logo, { scale: 3.5 }, { scale: 3, duration: 5, delay: 1.5, ease: "power2.out" });
+  });  
+
 
   // wave svg
-  gsap.fromTo(
-    waveSvg,
-    {
-      y: "100%",
-    },
-    {
-      y: "0%", 
-      duration: 2,
-      ease: "power2.out",
-      delay: 2.3 
-    }
-  );
+  gsap.matchMedia().add(breakpoints.desktop, () => {
+    gsap.fromTo(
+      waveSvg,
+      {
+        y: "100%",
+      },
+      {
+        y: 80, 
+        duration: 2,
+        ease: "power2.out",
+        delay: 2
+      }
+    );
+  });
+
+  gsap.matchMedia().add(breakpoints.smallLaptop, () => {
+    gsap.fromTo(
+      waveSvg, {y: "100%",}, {y: 30, duration: 2, delay: 2, ease: "power2.out"}
+    )
+  });
+
+  gsap.matchMedia().add(breakpoints.tablet, () => {
+    gsap.fromTo(
+      waveSvg, {y: "100%",}, {y: -20, duration: 2, delay: 2, ease: "power2.out"}
+    )
+  });
+
+  gsap.matchMedia().add(breakpoints.largePhone, () => {
+    gsap.fromTo(
+      waveSvg, {y: "100%",}, {y: -70, duration: 2, delay: 2, ease: "power2.out"}
+    )
+  });
+
+  gsap.matchMedia().add(breakpoints.smallPhone, () => {
+    gsap.fromTo(
+      waveSvg, {y: "100%",}, {y: -80, duration: 2, delay: 2, ease: "power2.out"}
+    )
+  });
+  // --------------
+  // end section 1
+  // --------------
 
 });
+
+
+
+
 
 // svg
 var container = document.body;
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,16 +6,19 @@
   <!-- Bagian Tengah (Tulisan + Logo) -->
   <div class="d-flex align-items-center justify-content-center">
     <!-- Tulisan OSPEK -->
-    <h1 id="text-kiri" class="me-4 text-uppercase fw-bold" style="font-size: 10rem;">OSPEK</h1>
+    <h1 id="text-kiri" class="me-4 text-uppercase fw-bold">OSPEK</h1>
 
     <!-- Gambar -->
-    <div class="logo-ospek align-self-center">
+    {{-- <div class="logo-ospek align-self-center">
       <img id="logo-ospek" src="{{ asset('assets/home/logo_2.png') }}" alt="" 
             class="img-fluid" style="width: 200px; height: auto;">
-    </div>
+    </div> --}}
+
+    <img id="logo-ospek" src="{{ asset('assets/home/logo_2.png') }}" alt="" 
+            class="img-fluid">
 
     <!-- Tulisan 2025 -->
-    <h1 id="text-kanan" class="ms-4 text-uppercase fw-bold" style="font-size: 10rem;">2 0 2 5</h1>
+    <h1 id="text-kanan" class="ms-4 text-uppercase fw-bold">2 0 2 5</h1>
   </div>
   <!-- Wave SVG (di bawah elemen lainnya) -->
   <svg class="wave-svg" width="100%" height="50%" version="1.1" xmlns="http://www.w3.org/2000/svg" id="intro">
