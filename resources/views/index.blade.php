bfc4e03a1973b8856434bf80ab4065e7f65ee983
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2025-02-17 22:32:35 +0700

    [rizky] -> add limit(wave) in home-section-4

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -39,11 +39,12 @@
     </div>
   </section>
   {{-- end section 2 --}}
+
   
   {{-- section 3 --}}
   <section class="home-section-3" id="home-section-3" data-scroll-container>
     <svg class="wave-svg-2" width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg" id="intro-2">
-      <path id="wave-2" d="" fill="#317F80"/>
+      <path id="wave-2" d="" fill="#122b26"/>
     </svg>  
     
     <div>
@@ -64,6 +65,14 @@
 
   {{-- section 4 --}}
   <section class="home-section-4" id="home-section-4">
+
+    {{-- batas --}}
+    <div class="custom-shape-divider-top-1739805712">
+      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+      </svg>
+    </div>
+
     <div class="logo-scroll-container">
       <section class="logo-section d-flex align-items-center panel">
         <div class="container">
